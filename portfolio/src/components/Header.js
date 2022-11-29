0b3ede0aa1f00640c9cd78d5d41c2d423950b8ef
Alana Correia <alanacorreia.ac@gmail.com>
2022-11-29 11:44:06 -0300

    feat: configurações iniciais react/styled-components

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <p>AC</p>
+      <div>
+        <p>Sobre</p>
+        <p>Skills</p>
+        <p>Projetos</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
