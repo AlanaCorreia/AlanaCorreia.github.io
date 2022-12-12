d794375914b8e83b81c7d7076ce5178f6a1efda5
Alana Correia <alanacorreia.ac@gmail.com>
2022-12-12 14:24:04 -0300

    fix: corrige tamanho da página de projetos

--- portfolio/src/styles/Skills.jsx
+++ portfolio/src/styles/Skills.jsx
@@ -3,8 +3,7 @@ import { BROWN } from './Colors';
 import background from '../images/background.jpg';
 
 export const Container = styled.div`
-  height: 100vh;
-
+  height: ${({projects}) => projects ? `auto` : '100vh'};
   background-image: url(${background});
   display: flex;
   flex-direction: column;
